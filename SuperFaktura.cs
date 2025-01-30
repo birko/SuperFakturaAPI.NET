2d708dcd430b2aa32ad8f5781ee934acf97fcb3c
František Bereň <birko@live.com>
2025-01-30 08:40:44 +0100

    Typos fix

--- SuperFaktura.cs
+++ SuperFaktura.cs
@@ -595,7 +595,7 @@ namespace Birko.SuperFaktura
             APIURL = "https://moja.superfaktura.sk/";
         }
 
-        [Obsolete("Not fount in API documentation")]
+        [Obsolete("Not found in API documentation")]
         public async Task<Response<Register>> Register(string email, bool sendEmail = true)
         {
             var result = await Post("users/create", new Request.UserData
@@ -609,7 +609,7 @@ namespace Birko.SuperFaktura
             return DeserializeResult<Response<Register>>(result);
         }
 
-        [Obsolete("Not fount in API documentation")]
+        [Obsolete("Not found in API documentation")]
         public async Task<Response<ExpandoObject>> GetCourierData(string curierType, object data)
         {
             if (new[] { "slp", "csp" }.Contains(curierType))
@@ -620,7 +620,7 @@ namespace Birko.SuperFaktura
             return null;
         }
 
-        [Obsolete("Not fount in API documentation")]
+        [Obsolete("Not found in API documentation")]
         public async Task<string> ResponseByChecksum(string checksum)
         {
             return await Get($"api_logs/getResponseByChecksum/{checksum}").ConfigureAwait(false);
